7475fb04b280d46f395326b06b27e6e90e6fd0f8
Agustin Higinio <agustin@creativedog.agency>
2022-04-26 20:54:45 -0300

    Clase 3 - JSX Ejemplos

--- Clase 2 - Instalacion/miproyecto2/src/App.js
+++ Clase 2 - Instalacion/miproyecto2/src/App.js
@@ -1,9 +1,46 @@
 
 
 function App() {
+  /* ----------------------------- Spread operator ---------------------------- */
+  const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const array2 = [...array1, 11, 12, 13, 14, 15];
+  //console.log(array2)
+
+  // Destructuring con objeto
+   const objeto = {
+    id: 1,
+    nombre: "Agustin"
+  }
+  const { nombre } = objeto;
+  console.log(nombre) 
+  /// Esto es lo mismo que realizar console.log(objecto.nombre)
+  
+  // Destructuring con array
+  const array3 = [
+    {
+      id: 1,
+      title: "Agustin"
+    },
+    {
+      id: 2,
+      title: "Juan"
+    }
+  ]
+  const { title } = array3[1];
+  console.log(title)
+  const contenedorStyle = {
+    backgroundColor: "#ccc"
+  }
   return (
-    <div className="App">
+    // <div style="border-color: red" class="App"></div>
+    <div style={contenedorStyle} className="App">
       <h1>Esta es mi primer aplicacion en React.JS</h1>
+      <p>Accedo a la propiedad Title del objecto en el array con posicion 1 {title}</p>
+      <p>Accedo a la propiedad Nombre del objeto {nombre}</p>
+      <h2>Imprimo mi array</h2>
+      {array3.map((persona) => (
+        <p>{persona.title}</p>
+      ))}
     </div>
   );
 }
