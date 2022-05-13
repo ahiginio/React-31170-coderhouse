71eb4cab1eded9084a122e77f2878604c5fe12bd
Agustin Higinio <agustin@creativedog.agency>
2022-05-12 21:06:57 -0300

    Clase 7 - Routing

--- Clase 7 - Routing/ejemplos/src/App.js
+++ Clase 7 - Routing/ejemplos/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./views/Home/Home"
+import Cart from "./views/Cart/Cart"
+import Products from "./views/Products/Products"
+import Category from "./views/Category/Category"
+import Product from "./views/Product/Product"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="*" element={<Home />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/product/:productId" element={<Product />} />
+          <Route exact path="/category/:categoryId" element={<Category />} />
+          <Route exact path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
